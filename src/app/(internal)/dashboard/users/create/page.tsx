ae0aedc1e8aef936a6f3f5c2d31af0d6781e7089
michaelrynm <133634767+michaelrynm@users.noreply.github.com>
2026-02-26 20:56:45 +0700

    feat(user-managemenet):slicing create user page

---
+++ src/app/(internal)/dashboard/users/create/page.tsx
@@ -0,0 +1,33 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+
+
+import CreateUserForm from "@/features/internal/user-management/create-user-form";
+
+export default function CreateUser() {
+  return (
+    <div>
+      <p className="font-semibold">Create user</p>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink>User Management</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink>Create User</BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <CreateUserForm />
+    </div>
+  );
+}
